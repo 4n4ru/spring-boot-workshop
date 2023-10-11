6a1df2569528b163954b46f4549c9f950a3591e7
Ana Runje <ana.runje@clover.com>
2023-10-11 15:48:44 +0100

    first commit

---
+++ src/test/java/dev/runje/ana/springbootworkshop/SpringBootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.runje.ana.springbootworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
